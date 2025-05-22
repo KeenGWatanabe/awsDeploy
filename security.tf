110535a050441330218b53db4c925fcec12c96db
KeenGWatanabe <mckeenweb@gmail.com>
2025-05-23 06:15:59 +0800

    ecs tasks update

--- security.tf
+++ security.tf
@@ -16,6 +16,12 @@ resource "aws_security_group" "ecs_tasks" {
     to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
+   egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Or restrict to AWS IP ranges
+  }
 }
 
 # Security group for VPC endpoints
